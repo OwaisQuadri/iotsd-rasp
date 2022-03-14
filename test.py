aab5f8d31be27dd4e12a816b46583917359214c4
Owais Quadri Home PC <owais.quadri@ontariotechu.net>
2022-03-13 21:13:37 -0400

    figuring out gpio with new hardware

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ GPIO.setmode(GPIO.BCM)
 ledGPIO=18
 led2GPIO=23
 GPIO.setup(ledGPIO, GPIO.OUT)
+GPIO.setup(led2GPIO, GPIO.OUT)
 #lock
 lockGPIO=21
 GPIO.setup(lockGPIO, GPIO.OUT)
@@ -15,8 +16,14 @@ GPIO.setup(pushbuttonGPIO, GPIO.IN)#maybe 20/25
 
 #run
 #accept inputs
-while True:
+# while True:
+    
+#     if GPIO.input(pushbuttonGPIO):
+#         print("button pressed")
 
-    if GPIO.input(pushbuttonGPIO):
-        print("button pressed")
+#toggle light
+GPIO.output(ledGPIO, True)
+GPIO.output(led2GPIO, True)
+GPIO.output(lockGPIO, True)
+
 
