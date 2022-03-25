ab9e37e439f29052f73a161ab5b0fbdff7ec5c63
Owais Quadri <owais.quadri@ontariotechu.net>
2022-03-25 14:09:07 -0400

    test

--- test.py
+++ test.py
@@ -1,37 +1,40 @@
 import RPi.GPIO as GPIO
 import time
 import pyttsx3
-#setup speech synthesis
-synthesizer=pyttsx3.init()
+# setup speech synthesis
+synthesizer = pyttsx3.init()
 # setup gpio
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 # led
-ledGPIO = 18
-led2GPIO = 23
-GPIO.setup(ledGPIO, GPIO.OUT)
-GPIO.setup(led2GPIO, GPIO.OUT)
+redLED = 14
+greenLED = 15
+GPIO.setup(redLED, GPIO.OUT)
+GPIO.setup(greenLED, GPIO.OUT)
 # lock
-lockGPIO = 21
+lockGPIO = 23
 GPIO.setup(lockGPIO, GPIO.OUT)
 # button
-pushbuttonGPIO = 25
+pushbuttonGPIO = 18
 GPIO.setup(pushbuttonGPIO, GPIO.IN)  # maybe 20/25
 
 # run
 # accept inputs
 try:
-    synthesizer.say("Here we go")
+    synthesizer.say("Here we go again")
     synthesizer.runAndWait()
     synthesizer.stop()
     while True:
         # make light and lock False
-        GPIO.output(ledGPIO, True)
-        GPIO.output(led2GPIO, True)
+        GPIO.output(redLED, True)
+        GPIO.output(greenLED, True)
         GPIO.output(lockGPIO, )
         if GPIO.input(pushbuttonGPIO):
             print("button pressed")
+            synthesizer.say("Nice!")
+            synthesizer.runAndWait()
+            synthesizer.stop()
 except:
-    GPIO.output(ledGPIO, False)
-    GPIO.output(led2GPIO, False)
+    GPIO.output(redLED, False)
+    GPIO.output(greenLED, False)
     GPIO.output(lockGPIO, False)
